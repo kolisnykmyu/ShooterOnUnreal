988174fb187d2a292ed7cc0f99d9fd9ace673eb5
Mykola Kolisnyk <kolisnykmyu@gmail.com>
2023-02-16 22:24:25 +0200

    SOU-7 add autoheal

--- Source/ShooterOnUnreal/Private/Components/SOUHealthComponent.cpp
+++ Source/ShooterOnUnreal/Private/Components/SOUHealthComponent.cpp
@@ -2,6 +2,8 @@
 
 #include "Components/SOUHealthComponent.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
 
@@ -15,8 +17,7 @@ void USOUHealthComponent::BeginPlay()
 {
     Super::BeginPlay();
 
-    Health = MaxHealth;
-    OnHealthChanged.Broadcast(Health);
+    SetHealth(MaxHealth);
 
     AActor* ComponentOwner = GetOwner();
     if (ComponentOwner)
@@ -28,12 +29,35 @@ void USOUHealthComponent::BeginPlay()
 void USOUHealthComponent::OnTakeDamage(
     AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
-    if (Damage <= 0.0f || IsDead())
+    if (Damage <= 0.0f || IsDead() || !GetWorld())
         return;
-    Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
+    SetHealth(Health - Damage);
     OnHealthChanged.Broadcast(Health);
+
+    GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+
     if (IsDead())
     {
         OnDeath.Broadcast();
     }
+    else if (AutoHeal)
+    {
+        GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USOUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
+    }
+}
+
+void USOUHealthComponent::HealUpdate()
+{
+    SetHealth(Health + HealModifier);
+    
+    if( FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld())
+    {
+        GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+    }
+}
+
+void USOUHealthComponent::SetHealth(float NewHealth)
+{
+    Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+    OnHealthChanged.Broadcast(Health);
 }
